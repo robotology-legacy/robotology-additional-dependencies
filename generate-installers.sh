d215b76e3f1e7121fc0a96b1f8823b1aa6826a7b
Silvio Traversaro <silvio.traversaro@iit.it>
2019-09-10 15:13:46 +0200

    Update installer to use 2019.08 vcpkg version

--- generate-installers.sh
+++ generate-installers.sh
@@ -16,7 +16,7 @@ mkdir build
 
 # Copy the appropriate version of vcpkg-cpack
 cd build
-git clone https://github.com/robotology-playground/vcpkg-cpack -b add-internal-vcpkg
+git clone https://github.com/robotology-playground/vcpkg-cpack -b use-internal-vcpkg
 cd ..
 
 # Create x86-windows installers 
@@ -41,7 +41,7 @@ cd ../..
 echo "Copying installers in artifacts directory"
 rm -rf artifacts
 mkdir artifacts
-RAD_VERSION="0.2.0"
+RAD_VERSION="0.4.0"
 cp ./build/x86-windows/robotology-additional-dependencies-${RAD_VERSION}-x86.exe ./artifacts/robotology-additional-dependencies-${RAD_VERSION}-v141-x86.exe
 cp ./build/x86-windows/robotology-additional-dependencies-${RAD_VERSION}-x86.zip ./artifacts/robotology-additional-dependencies-${RAD_VERSION}-v141-x86.zip
 cp ./build/x64-windows/robotology-additional-dependencies-${RAD_VERSION}-x86_amd64.exe ./artifacts/robotology-additional-dependencies-${RAD_VERSION}-v141-x86_amd64.exe
